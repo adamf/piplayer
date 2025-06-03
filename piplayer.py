9478720948a79dd9af8e7e72e37a29b2ad4358b9
Adam Fletcher <adamfblahblah@gmail.com>
2025-06-02 19:04:52 -0700

    Add previous track and logging.

--- piplayer.py
+++ piplayer.py
@@ -7,6 +7,12 @@ import threading
 import time
 import keyboard
 import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+# On OS X, install portaudio and pyaudio, or playback is pretty slow.
+# brew install ffmpeg portaudio && pip install pyaudio
 
 MUSIC_DIR = "/home/music/music/"
 
@@ -26,6 +32,7 @@ class Player():
     music_dir = None
     timer = None
     track_time_remaining = 0
+
 
     def __init__(self, music_dir):
         self.player = Playback()
@@ -47,16 +54,16 @@ class Player():
             self.playlist.remove(mp3_file)
 
     def pause(self):
-        print("pause", self.timer.remaining(), self.timer.elapsed())
+        logger.info(f"pause: remaining={self.timer.remaining():.2f}, elapsed={self.timer.elapsed():.2f}")
         self.track_time_remaining = self.timer.remaining()
         self.timer.cancel()
         self.player.pause()
 
 
     def play(self):
-        print("play")
+        logger.info("play")
         if self.player.paused:
-            print("play, but we are paused")
+            logger.info("play, but we are paused")
             self.player.resume()
             self.timer = PlayerTimer(self.track_time_remaining, self.next_track)
             self.timer.start()
@@ -64,9 +71,9 @@ class Player():
             self.next_track()
 
     def next_track(self):
-        print("next_track")
+        logger.info("next_track")
         mp3_file = self.playlist.pop(0)
-        print(mp3_file)
+        logger.info(mp3_file)
         self.playlist.append(mp3_file)
         self.player.load_file(mp3_file)
         self.player.play()
@@ -75,7 +82,21 @@ class Player():
         self.timer = PlayerTimer(self.player.duration, self.next_track)
         self.timer.start()
         self.load_playlist()
-    
+
+    def previous_track(self):
+        logger.info("previous_track")
+        mp3_file = self.playlist.pop()  # Take the last track
+        logger.info(mp3_file)
+        self.playlist.insert(0, mp3_file)  # Move it to the front
+        self.player.load_file(mp3_file)
+        self.player.play()
+        if self.timer:
+                self.timer.cancel()
+        self.timer = PlayerTimer(self.player.duration, self.next_track)
+        self.timer.start()
+        self.load_playlist()
+
+
 def on_release(key, player):
     if key == 'play':
         if player.player.playing:
@@ -86,18 +107,14 @@ def on_release(key, player):
             player.play()
     if key == 'next' or not player.player.active:
         player.next_track()
+    if key == 'previous' or not player.player.active:
+        player.previous_track()
 
 
 player = Player(MUSIC_DIR)
 
 keyboard.add_hotkey('a', on_release, args=['play', player])
 keyboard.add_hotkey('b', on_release, args=['next', player])
+keyboard.add_hotkey('c', on_release, args=['previous', player])
 
 keyboard.wait('esc')
-
-
-
-
-
-
-
